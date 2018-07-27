1e669409f375cebf29abdbc084a10abf39eab7d0
Samyeoul Choi <sam.yeoul.choi1@hp.com>
2018-07-27 10:57:36 +0900

    First commit

---
+++ app/src/main/java/workandlife/com/workandlife/MainActivity.java
@@ -0,0 +1,63 @@
+package workandlife.com.workandlife;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 1;
+
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getApplicationContext();
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate");
+
+        checkPermission();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();;
+        stopService(new Intent(MainActivity.this, KeyListenerService.class));
+    }
+
+    public void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {   // 마시멜로우 이상일 경우
+            if (!Settings.canDrawOverlays(this)) {              // 체크
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE);
+            } else {
+                startService(new Intent(MainActivity.this, KeyListenerService.class));
+            }
+        } else {
+            startService(new Intent(MainActivity.this, KeyListenerService.class));
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE) {
+            if (!Settings.canDrawOverlays(this)) {
+                // TODO 동의를 얻지 못했을 경우의 처리
+
+            } else {
+                startService(new Intent(MainActivity.this, KeyListenerService.class));
+            }
+        }
+    }
+}
